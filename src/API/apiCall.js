21e020479dfbb0b4db3630a3e9a7952ed96c07ea
hiteshv <hiteshvijay7@gmail.com>
2021-06-08 15:32:42 +0530

    USE: react-markdown to render markdown text

--- src/API/apiCall.js
+++ src/API/apiCall.js
@@ -1,6 +1,7 @@
 import Axios from 'axios'
 
-const API_ENDPOINT = 'https://gist.githubusercontent.com/hiteshvijayhv/2b26e64f2c1f28315441b2a6500ddfb8/raw/e0c7b7ce0262e2a2c0e505154fad0d21060d3aa4/blogs.json'
+//const API_ENDPOINT = 'https://gist.githubusercontent.com/hiteshvijayhv/2b26e64f2c1f28315441b2a6500ddfb8/raw/e0c7b7ce0262e2a2c0e505154fad0d21060d3aa4/blogs.json'
+const API_ENDPOINT = 'https://gist.githubusercontent.com/hiteshvijayhv/229562bc4cdb915bf26d340aae98b0b7/raw/7de6c7cef972050b29610863c1659e33c5592b7e/testingblog.json'
 export const getResponse = async() => {
     
     var response;
--- src/components/PostDisplay.js
+++ src/components/PostDisplay.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { getResponse } from '../API/apiCall'
+import ReactMarkdown from 'react-markdown';
 
 function PostDisplay({ match }) {
 
@@ -17,6 +18,7 @@ function PostDisplay({ match }) {
         setApiData(data)
         console.log(data)
     }
+    const markdown = apiData.data.blogs[pos].post
   
     
     return (
@@ -25,6 +27,8 @@ function PostDisplay({ match }) {
             {apiData.data && (
                     <div>
                           <h1>{apiData.data.blogs[pos].title}</h1>
+                          <p>{apiData.data.blogs[pos].post}</p>
+                          <p><ReactMarkdown children={markdown} /></p>
                     </div>
                 )}
             </h1>
