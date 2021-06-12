7627dbb597edf9ce686b6a6a6f0133be527f7b69
hiteshv <hiteshvijay7@gmail.com>
2021-06-12 11:46:04 +0530

    Make pages responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
 
     return (
-        <div>
-            <h1></h1>
+        <div className='main-container'>
+            <div className='home-center'>
+
+                <div className='img-div'>
+                    <img className='profile-img'
+                        src='https://user-images.githubusercontent.com/46933160/121765118-5d754000-cb66-11eb-8b52-33f477b49a6e.jpg'>
+                    </img>
+                </div>
+
+                    <div className='info-div'>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                         standard dummy 
+                    </div>
+                
+                </div>
         </div>
     )
 }
