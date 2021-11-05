3df67b913193edcde5990d5d8d1bfab15e98fb78
hiteshv <hiteshvijay7@gmail.com>
2021-11-05 09:37:29 +0530

    Update styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,38 @@
 import React from 'react'
 import './Home.css'
 
+
 function Home() {
 
     return (
         <div className='main-container'>
             <div className='home-center'>
 
-                <div className='img-div'>
-                    <img className='profile-img'
-                        src='https://user-images.githubusercontent.com/46933160/121765118-5d754000-cb66-11eb-8b52-33f477b49a6e.jpg'>
-                    </img>
-                </div>
-
                     <div className='info-div'>
-                         <p>Hey👋, I am a Engineering undergrad who is exploring web development.
-                              The technologies I use are React for the front end and Node with Express for the server side of things.</p>
+                         <p className="intro">Hey👋, I am Hitesh</p>
+                         <p className="intro-description">I enjoy building web applications and solving problems
                          <div className='social'>
-                             <a href='https://twitter.com/hiteshvijayhv' target="_blank">
+                            <a href='https://twitter.com/hiteshvijayhv' target="_blank">
                                  <img  className='icon' src='https://user-images.githubusercontent.com/46933160/121767463-ef387980-cb75-11eb-9413-b32129def4ce.png'>
 
                                  </img>
                              </a>
                              <a href='https://github.com/hiteshvijayhv' target="_blank">
                                  <img  className='icon' src='https://user-images.githubusercontent.com/46933160/121767611-d1b7df80-cb76-11eb-9c96-f16cc16214ec.png'>
-
                                  </img>
                              </a>
                     
-                         </div>
+                            </div> 
+                         
+                         </p>
+
+                         
+                         
+                    </div>
+                    <div className='img-div'>
+                    <img className='profile-img'
+                        src='https://user-images.githubusercontent.com/46933160/121765118-5d754000-cb66-11eb-8b52-33f477b49a6e.jpg'>
+                    </img>
                     </div>
                 
                 </div>
@@ -37,3 +41,5 @@ function Home() {
 }
 
 export default Home
+
+{/* */}
